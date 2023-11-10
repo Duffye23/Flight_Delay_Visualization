481b724868b9fa6642ca212f12bace2fc14fd30b
Duffye23 <evan-duffy@live.com>
2023-11-09 21:28:24 -0500

    fixed errors in html and jsonified

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, request, redirect, url_for
 import psycopg2
 from flask import jsonify
+import json
+import pandas as pd
+from json import loads, dumps
 
 #Connect to the database
 conn = psycopg2.connect(database="Project3_db",
@@ -13,6 +16,18 @@ cur = conn.cursor()
 
 conn.commit()
 
+#Select all flights from the table
+cur.execute('''SELECT * FROM flights LIMIT 10000''')
+
+#Fetch that data we just selected
+data = cur.fetchall()
+data = pd.DataFrame(data)
+data = data.drop([0,18,19,20],axis=1)
+data.columns = ['airline', 'origin','destination','departure time', 'initial departure time', 'taxi time', 'departure delay', 'air time', 'distance', 'cancelled', 'tail number', 'ICAO Type', 'manufacture year', 'temperature', 'cloud cover', 'active weather', 'id']
+result = data.to_json(orient="index")
+data = loads(result)
+dumps(data, indent=4) 
+#print(data)
 cur.close()
 conn.close()
 
@@ -20,28 +35,14 @@ conn.close()
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    #Connect to the database
-    conn = psycopg2.connect(database="Project3_db",
-                        user="postgres",
-                        password="postgres",
-                        host="localhost", port="5432"
-                        )
-    #create the cursor for the webpage
-    cur = conn.cursor()
+def index():      
+    return render_template('index.html')
 
-    #Select all flights from the table
-    cur.execute('''SELECT * FROM flights LIMIT 50000''')
+@app.route('/process_data', methods=['POST'])
+def retrieve():
+    data = data
+    return jsonify(data=data)
 
-    #Fetch that data we just selected
-    data = cur.fetchall()
-
-    #close the cursor and connection
-    cur.close()
-    conn.close()
-
-    
-    return render_template('index.html', data=data)
 
 if __name__ == '__main__':
     app.run(debug=True)
